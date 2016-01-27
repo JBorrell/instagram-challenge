0110bbccaa1cfa871af8814cb67de1977f312a6e
James Borrell <james.borrell92@gmail.com>
2016-01-27 22:28:28 +0000

    Form for new post created, new & created methods added in Post Controller, green tests

--- insta_pik/spec/features/posts_feature_spec.rb
+++ insta_pik/spec/features/posts_feature_spec.rb
@@ -11,10 +11,23 @@ feature 'posts' do
       expect(page).to have_link('Post!')
     end
   end
-  # context 'adding a post' do
-  #   scenario 'site displays added post on submit' do
-  #     visit '/posts'
-  #     click_link('Post!')
-  #   end
-  # end
+  context 'showing a post' do
+    scenario 'site displays posts on homepage' do
+      Post.create(caption: 'Caption')
+      visit '/posts'
+      expect(page).to have_content("Caption")
+      expect(page).not_to have_content("No Posts Yet")
+    end
+  end
+  context 'creating a post' do
+    scenario "'Post!' link takes you to a form to add a caption on a post" do
+      visit '/posts'
+      click_link('Post!')
+      fill_in 'Caption', with: "Insta-Pik!"
+      click_button('Create Post')
+      expect(page).to have_content("Insta-Pik!")
+      expect(page).not_to have_content("No Posts Yet")
+      expect(current_path).to eq '/posts'
+    end
+  end
 end
